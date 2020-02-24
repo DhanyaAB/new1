9718419078bc64a7b4f0d90c863512119d9bcbd9
ASM <dhanuab15@gmail.com>
2020-02-24 15:22:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './home/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      component: MainComponent,
+      // children: [
+      //   { path: 'dashboard', component: DashboardComponent },
+      //   { path: 'night/image/:userId', component: BuildoneComponent },
+      //   { path: 'private/image/:userId', component: PrivateComponent },
+      //   { path: 'redirect', component: RedirectComponent },
+      //   { path: 'testUI', component:TestUIComponent}
+      // ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { MainComponent } from './home/main/main.component';
+import { SidebarComponent } from './home/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { ToastrModule } from 'ngx-toastr';
+//import { CollapseModule } from 'ngx-bootstrap/collapse';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+
+    BrowserAnimationsModule,
+    // CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
